c502799f7ec76b61778862d2979fb3acaeddb8aa
Nuh Akturk <nuhar42@gmail.com>
2022-09-27 18:40:54 +0300

    Folder Browser Dialog dersi uygulama

---
+++ C#_Form_ile_Gorsel_ve_Nesne_Tabanli_Programlama/Dosya_islemleri/Dosya_islemleri/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Dosya_islemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Btn_KonumSec = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_KonumSec
+            // 
+            this.Btn_KonumSec.Location = new System.Drawing.Point(562, 85);
+            this.Btn_KonumSec.Name = "Btn_KonumSec";
+            this.Btn_KonumSec.Size = new System.Drawing.Size(225, 47);
+            this.Btn_KonumSec.TabIndex = 0;
+            this.Btn_KonumSec.Text = "Konum Seç";
+            this.Btn_KonumSec.UseVisualStyleBackColor = true;
+            this.Btn_KonumSec.Click += new System.EventHandler(this.Btn_KonumSec_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 511);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_KonumSec);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button Btn_KonumSec;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
