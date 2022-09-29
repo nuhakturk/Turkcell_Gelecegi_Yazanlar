ee6e768d8a8a8756267cec3583469f7ccf478e1e
Nuh Akturk <nuhar42@gmail.com>
2022-09-29 08:24:59 +0300

    Login islemi dersi uygulamasi

--- C#_Form_ile_Gorsel_ve_Nesne_Tabanli_Programlama/Form_islemleri/Form_islemleri/Form1.Designer.cs
+++ C#_Form_ile_Gorsel_ve_Nesne_Tabanli_Programlama/Form_islemleri/Form_islemleri/Form1.Designer.cs
@@ -30,13 +30,16 @@ namespace Form_islemleri
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtKullaniciAd = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label3 = new System.Windows.Forms.Label();
+            this.TxtKod1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtKod2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -48,39 +51,40 @@ namespace Form_islemleri
             this.label1.TabIndex = 0;
             this.label1.Text = "Kullanıcı Adınız:";
             // 
-            // textBox1
+            // TxtKullaniciAd
             // 
-            this.textBox1.Location = new System.Drawing.Point(203, 137);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(305, 30);
-            this.textBox1.TabIndex = 1;
+            this.TxtKullaniciAd.Location = new System.Drawing.Point(203, 137);
+            this.TxtKullaniciAd.Name = "TxtKullaniciAd";
+            this.TxtKullaniciAd.Size = new System.Drawing.Size(305, 30);
+            this.TxtKullaniciAd.TabIndex = 1;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(203, 294);
+            this.checkBox1.Location = new System.Drawing.Point(203, 328);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(133, 29);
-            this.checkBox1.TabIndex = 5;
+            this.checkBox1.TabIndex = 6;
             this.checkBox1.Text = "Beni Hatırla";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 225);
+            this.button1.Location = new System.Drawing.Point(203, 259);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 49);
-            this.button1.TabIndex = 3;
+            this.button1.TabIndex = 4;
             this.button1.Text = "Giriş Yap";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // TxtSifre
             // 
-            this.textBox2.Location = new System.Drawing.Point(203, 173);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(305, 30);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.TxtSifre.Location = new System.Drawing.Point(203, 173);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(305, 30);
+            this.TxtSifre.TabIndex = 2;
+            this.TxtSifre.UseSystemPasswordChar = true;
             // 
             // label2
             // 
@@ -95,10 +99,10 @@ namespace Form_islemleri
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.LinkColor = System.Drawing.Color.Black;
-            this.linkLabel1.Location = new System.Drawing.Point(358, 237);
+            this.linkLabel1.Location = new System.Drawing.Point(358, 271);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(150, 25);
-            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabIndex = 5;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Şifremi Unuttum";
             // 
@@ -113,19 +117,46 @@ namespace Form_islemleri
             this.label3.TabIndex = 8;
             this.label3.Text = "Admin Login Form";
             // 
+            // TxtKod1
+            // 
+            this.TxtKod1.Location = new System.Drawing.Point(203, 209);
+            this.TxtKod1.Name = "TxtKod1";
+            this.TxtKod1.Size = new System.Drawing.Size(133, 30);
+            this.TxtKod1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Kodu Girin:";
+            // 
+            // TxtKod2
+            // 
+            this.TxtKod2.Enabled = false;
+            this.TxtKod2.Location = new System.Drawing.Point(363, 209);
+            this.TxtKod2.Name = "TxtKod2";
+            this.TxtKod2.Size = new System.Drawing.Size(145, 30);
+            this.TxtKod2.TabIndex = 34;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(590, 371);
+            this.ClientSize = new System.Drawing.Size(585, 375);
+            this.Controls.Add(this.TxtKod2);
+            this.Controls.Add(this.TxtKod1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TxtSifre);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtKullaniciAd);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -133,6 +164,7 @@ namespace Form_islemleri
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,13 +173,16 @@ namespace Form_islemleri
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtKullaniciAd;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TxtSifre;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtKod1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtKod2;
     }
 }
 
--- C#_Form_ile_Gorsel_ve_Nesne_Tabanli_Programlama/Form_islemleri/Form_islemleri/Form1.cs
+++ C#_Form_ile_Gorsel_ve_Nesne_Tabanli_Programlama/Form_islemleri/Form_islemleri/Form1.cs
@@ -16,5 +16,30 @@ namespace Form_islemleri
         {
             InitializeComponent();
         }
+        void kodolustur()
+        {
+            Random rastgele = new Random();
+            int sayi = rastgele.Next(10000, 100000);
+            TxtKod2.Text = sayi.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kodolustur();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(TxtKullaniciAd.Text == "Admin" && TxtSifre.Text == "1234" && TxtKod1.Text == TxtKod2.Text)
+            {
+                Form2 frm = new Form2();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı bilgi girişi: Kullanıcı adı, şifre veya kod yanlış girildi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
     }
 }
