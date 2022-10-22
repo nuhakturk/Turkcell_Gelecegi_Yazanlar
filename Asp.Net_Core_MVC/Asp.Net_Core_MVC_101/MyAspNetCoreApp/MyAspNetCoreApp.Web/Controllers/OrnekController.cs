536c2dda8ff1288b08a6bf7f65fc69fac31d0897
Nuh Akturk <nuhar42@gmail.com>
2022-10-22 17:50:56 +0300

    Action Method Parametre Tanimlama dersi uygulama

--- Asp.Net_Core_MVC/Asp.Net_Core_MVC_101/MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/OrnekController.cs
+++ Asp.Net_Core_MVC/Asp.Net_Core_MVC_101/MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/OrnekController.cs
@@ -11,8 +11,20 @@ namespace MyAspNetCoreApp.Web.Controllers
 
         public IActionResult Index2()
         {
-
+            //veritabanı kaydetme işlemi
             return RedirectToAction("Index", "Ornek");
+        }
+
+        public IActionResult ParametreView(int id)
+        {
+
+
+            return RedirectToAction("JsonResultParametre", "Ornek", new { id = id });
+        }
+
+        public IActionResult JsonResultParametre(int id)
+        {
+            return Json(new { Id = id });
         }
 
         public IActionResult ContentResult()
