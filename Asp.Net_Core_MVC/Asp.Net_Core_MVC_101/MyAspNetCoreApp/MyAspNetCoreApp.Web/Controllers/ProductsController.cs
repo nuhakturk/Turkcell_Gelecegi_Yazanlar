4a3d63b3623c3f1e38a1cff6199a04783a812a92
Nuh Akturk <nuhar42@gmail.com>
2022-10-27 12:07:52 +0300

    ProductController Sinifinin Kodlanmasi dersi uyg

---
+++ Asp.Net_Core_MVC/Asp.Net_Core_MVC_101/MyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly ProductRepository _productRepository;
+		public ProductsController()
+		{
+			_productRepository = new ProductRepository();
+
+			if (!_productRepository.GetAll().Any())
+			{
+                _productRepository.Add(new() { Id = 1, Name = "Kalem 1", Price = 100, Stock = 200 });
+                _productRepository.Add(new() { Id = 2, Name = "Kalem 2", Price = 200, Stock = 300 });
+                _productRepository.Add(new() { Id = 3, Name = "Kalem 3", Price = 300, Stock = 400 });
+            }
+			
+        }
+		public IActionResult Index()
+		{
+			var products = _productRepository.GetAll();
+
+			return View(products);
+		}
+	}
+}
