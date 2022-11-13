c76cb68d651ff2b47e194bcd47813cad1f90087f
Nuh Akturk <nuhar42@gmail.com>
2022-11-13 23:08:15 +0300

    Transient dersi uygulama

--- Asp.Net_Core_MVC/Asp.Net_Core_MVC_201/TurkcellMyAspNetCoreApp/MyAspNetCoreApp.Web/Helpers/Helper.cs
+++ Asp.Net_Core_MVC/Asp.Net_Core_MVC_201/TurkcellMyAspNetCoreApp/MyAspNetCoreApp.Web/Helpers/Helper.cs
@@ -1,7 +1,15 @@
-﻿namespace MyAspNetCoreApp.Web.Helpers
+﻿using MyAspNetCoreApp.Web.Models;
+
+namespace MyAspNetCoreApp.Web.Helpers
 {
     public class Helper : IHelper
     {
+        private readonly AppDbContext _context;
+        public Helper(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public string Upper(string text)
         {
             return text.ToUpper();
