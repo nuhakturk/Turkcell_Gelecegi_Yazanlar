f4945b46a42370c17c3162e84a546fc687127206
Nuh Akturk <nuhar42@gmail.com>
2022-11-30 16:30:47 +0300

    Model/Form Validation Range dersi uygulama

--- Asp.Net_Core_MVC/Asp.Net_Core_MVC_301/TurkcellMyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
+++ Asp.Net_Core_MVC/Asp.Net_Core_MVC_301/TurkcellMyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -11,22 +11,24 @@ namespace MyAspNetCoreApp.Web.ViewModels
         [Required(ErrorMessage ="İsim alanı boş olamaz")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Fiyat alanı boş olamaz")]
+        [Required(ErrorMessage = "Fiyat alanı boş olamaz.")]
+        [Range(1, 1000, ErrorMessage = "Fiyat alanı 1 ile 1000 arasında bir değer olmalıdır.")]
         public decimal? Price { get; set; }
 
-        [Required(ErrorMessage = "Stok alanı boş olamaz")]
+        [Required(ErrorMessage = "Stok alanı boş olamaz.")]
+        [Range(1,200,ErrorMessage = "Stok alanı 1 ile 200 arasında bir değer olmalıdır.")]
         public int? Stock { get; set; }
 
-        [Required(ErrorMessage = "Açıklama alanı boş olamaz")]
+        [Required(ErrorMessage = "Açıklama alanı boş olamaz.")]
         public string Description { get; set; }
-        [Required(ErrorMessage = "Renk seçimi boş olamaz")]
+        [Required(ErrorMessage = "Renk seçimi boş olamaz.")]
         public string? Color { get; set; }
 
-        [Required(ErrorMessage = "Yayınlanma tarihi boş olamaz")]
+        [Required(ErrorMessage = "Yayınlanma tarihi boş olamaz.")]
         public DateTime? PublishDate { get; set; }
         public bool IsPublish { get; set; }
 
-        [Required(ErrorMessage = "Yayınlanma süresi boş olamaz")]
+        [Required(ErrorMessage = "Yayınlanma süresi boş olamaz.")]
         public int? Expire { get; set; }
     }
 }
