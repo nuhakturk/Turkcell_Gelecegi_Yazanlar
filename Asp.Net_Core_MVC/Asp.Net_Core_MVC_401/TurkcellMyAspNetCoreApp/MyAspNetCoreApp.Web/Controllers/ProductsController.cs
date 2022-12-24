70cbf086c7d8111bcd2c6cbf800591fd4e2f7c9b
Nuh Akturk <nuhar42@gmail.com>
2022-12-24 15:12:34 +0300

    ASP.NET-core-mvc-401 || Routing Yapisi dersi uyg

---
+++ Asp.Net_Core_MVC/Asp.Net_Core_MVC_401/TurkcellMyAspNetCoreApp/MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,181 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyAspNetCoreApp.Web.Helpers;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AppDbContext _context;
+
+        private readonly IMapper _mapper;
+        private readonly ProductRepository _productRepository;
+        public ProductsController(AppDbContext context, IMapper mapper)
+        {
+            _productRepository = new ProductRepository();
+            _context = context;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            
+            return View(_mapper.Map<List<ProductViewModel>>(products));
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            return View(_mapper.Map<ProductViewModel>(product));
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            _context.Products.Remove(product);
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Expire = new Dictionary<string, int>()
+            {
+                {"1 Ay", 1},
+                {"3 Ay", 3},
+                {"6 Ay", 6},
+                {"12 Ay", 12}
+            };
+
+            ViewBag.ColorSelect = new SelectList(new List<ColorSelectList>() { 
+                new() { Data="Mavi", Value="Mavi"},
+                new() { Data="Kırmızı", Value="Kırmızı"},
+                new() { Data="Sarı", Value="Sarı"}
+            }, "Value", "Data");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel newProduct)
+        {
+            //if (!string.IsNullOrEmpty(newProduct.Name) && newProduct.Name.StartsWith("A"))
+            //{
+            //    ModelState.AddModelError(String.Empty, "Ürün ismi A harfi ile başlayamaz");
+            //}
+            ViewBag.Expire = new Dictionary<string, int>()
+                {
+                    {"1 Ay", 1},
+                    {"3 Ay", 3},
+                    {"6 Ay", 6},
+                    {"12 Ay", 12}
+                };
+
+            ViewBag.ColorSelect = new SelectList(new List<ColorSelectList>() {
+                    new() { Data="Mavi", Value="Mavi"},
+                    new() { Data="Kırmızı", Value="Kırmızı"},
+                    new() { Data="Sarı", Value="Sarı"}
+                }, "Value", "Data");
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Products.Add(_mapper.Map<Product>(newProduct));
+                    _context.SaveChanges();
+
+                    TempData["status"] = "Ürün başarıyla eklendi.";
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError(String.Empty,"Ürün kaydedilirken bir hata meydana geldi. Lütfen daha sonra tekrar deneyiniz.");
+
+                    return View();
+                }
+                
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            ViewBag.ExpireValue = product.Expire;
+            ViewBag.Expire = new Dictionary<string, int>()
+            {
+                {"1 Ay", 1},
+                {"3 Ay", 3},
+                {"6 Ay", 6},
+                {"12 Ay", 12}
+            };
+
+            ViewBag.ColorSelect = new SelectList(new List<ColorSelectList>() {
+                new() { Data="Mavi", Value="Mavi"},
+                new() { Data="Kırmızı", Value="Kırmızı"},
+                new() { Data="Sarı", Value="Sarı"}
+            }, "Value", "Data", product.Color);
+
+            return View(_mapper.Map<ProductViewModel>(product));
+        }
+
+        [HttpPost]
+        public IActionResult Update(ProductViewModel updateProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ExpireValue = updateProduct.Expire;
+                ViewBag.Expire = new Dictionary<string, int>()
+                {
+                    {"1 Ay", 1},
+                    {"3 Ay", 3},
+                    {"6 Ay", 6},
+                    {"12 Ay", 12}
+                };
+
+                ViewBag.ColorSelect = new SelectList(new List<ColorSelectList>() {
+                    new() { Data="Mavi", Value="Mavi"},
+                    new() { Data="Kırmızı", Value="Kırmızı"},
+                    new() { Data="Sarı", Value="Sarı"}
+                }, "Value", "Data", updateProduct.Color);
+
+                return View();
+            }
+
+            _context.Products.Update(_mapper.Map<Product>(updateProduct));
+            _context.SaveChanges();
+            TempData["status"] = "Ürün başarıyla güncellendi.";
+            return RedirectToAction("Index");
+        }
+
+        [AcceptVerbs("GET","POST")]
+        public IActionResult HasProductName(string Name)
+        {
+            var anyProduct =_context.Products.Any(x => x.Name.ToLower() == Name.ToLower());
+
+            if(anyProduct)
+            {
+                return Json("Kaydetmeye çalıştığınız ürün ismi veritabanında bulunmaktadır.");
+            }
+            else
+            {
+                return Json(true);
+            }
+
+        }
+    }
+}
---
+++ Asp.Net_Core_MVC/Asp.Net_Core_MVC_401/TurkcellMyAspNetCoreApp/MyAspNetCoreApp.Web/ViewModels/ProductViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace MyAspNetCoreApp.Web.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Remote(action:"HasProductName", controller:"Products")]
+        [StringLength(50,ErrorMessage ="İsim alanına en fazla 50 karakter girilebilir.")]
+        [Required(ErrorMessage ="İsim alanı boş olamaz")]
+        public string? Name { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})",ErrorMessage ="Fiyat alanında noktadan sonra en fazla 2 basamak olmalıdır.")]
+        [Required(ErrorMessage = "Fiyat alanı boş olamaz.")]
+        [Range(1, 1000, ErrorMessage = "Fiyat alanı 1 ile 1000 arasında bir değer olmalıdır.")]
+        public decimal? Price { get; set; }
+
+        [Required(ErrorMessage = "Stok alanı boş olamaz.")]
+        [Range(1,200,ErrorMessage = "Stok alanı 1 ile 200 arasında bir değer olmalıdır.")]
+        public int? Stock { get; set; }
+
+        [StringLength(300,MinimumLength=50,ErrorMessage = "Açıklama alanı 50 ile 300 karakter arasında olabilir.")]
+        [Required(ErrorMessage = "Açıklama alanı boş olamaz.")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Renk seçimi boş olamaz.")]
+        public string? Color { get; set; }
+
+        [Required(ErrorMessage = "Yayınlanma tarihi boş olamaz.")]
+        public DateTime? PublishDate { get; set; }
+        public bool IsPublish { get; set; }
+
+        [Required(ErrorMessage = "Yayınlanma süresi boş olamaz.")]
+        public int? Expire { get; set; }
+    }
+}
