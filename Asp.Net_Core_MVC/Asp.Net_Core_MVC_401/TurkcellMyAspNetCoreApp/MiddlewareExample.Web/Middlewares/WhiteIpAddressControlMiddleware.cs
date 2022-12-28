5035d03c6b6f54ecde704484500d2af54df1b2ee
Nuh Akturk <nuhar42@gmail.com>
2022-12-28 23:22:55 +0300

    Middleware Ornek Senaryo-2.Kisim dersi uygulama

--- Asp.Net_Core_MVC/Asp.Net_Core_MVC_401/TurkcellMyAspNetCoreApp/MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
+++ Asp.Net_Core_MVC/Asp.Net_Core_MVC_401/TurkcellMyAspNetCoreApp/MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
@@ -1,20 +1,35 @@
-﻿namespace MiddlewareExample.Web.Middlewares
+﻿using System.Net;
+
+namespace MiddlewareExample.Web.Middlewares
 {
     public class WhiteIpAddressControlMiddleware
     {
         private readonly RequestDelegate _requestDelegate;
+        private const string WhiteIpAddress = "::1";
 
         public WhiteIpAddressControlMiddleware(RequestDelegate requestDelegate)
         {
             _requestDelegate = requestDelegate;
         }
 
-        public async Task InvokeAsync(HttpContent context)
+        public async Task InvokeAsync(HttpContext context)
         {
             //IPV4 => 127.0.0.1 => localhost
             //IPV6 => ::1 => localhost
 
+            var reqIpAddress = context.Connection.RemoteIpAddress;
 
+            bool AnyWhiteIpAddress = IPAddress.Parse(WhiteIpAddress).Equals(reqIpAddress);
+
+            if (AnyWhiteIpAddress == true)
+            {
+                await _requestDelegate(context);
+            }
+            else
+            {
+                context.Response.StatusCode = HttpStatusCode.Forbidden.GetHashCode();
+                await context.Response.WriteAsync("Forbidden");
+            }
         }
     }
 }
