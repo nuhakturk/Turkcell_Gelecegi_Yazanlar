ef51125747782030003655c79e81e2cb40bed7d0
Nuh Akturk <nuhar42@gmail.com>
2023-01-16 23:11:13 +0300

    Toplam Kategori Sayisini Bulma

--- C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.Designer.cs
+++ C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.Designer.cs
@@ -30,7 +30,7 @@
 		{
 			this.panel1 = new System.Windows.Forms.Panel();
 			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
+			this.LblToplamKategori = new System.Windows.Forms.Label();
 			this.panel2 = new System.Windows.Forms.Panel();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
@@ -65,7 +65,7 @@
 			// panel1
 			// 
 			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(160)))), ((int)(((byte)(133)))));
-			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.LblToplamKategori);
 			this.panel1.Controls.Add(this.label1);
 			this.panel1.Location = new System.Drawing.Point(12, 12);
 			this.panel1.Name = "panel1";
@@ -81,15 +81,15 @@
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Toplam Kategori Sayısı";
 			// 
-			// label2
+			// LblToplamKategori
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-			this.label2.Location = new System.Drawing.Point(100, 108);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(85, 29);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "label2";
+			this.LblToplamKategori.AutoSize = true;
+			this.LblToplamKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblToplamKategori.Location = new System.Drawing.Point(100, 108);
+			this.LblToplamKategori.Name = "LblToplamKategori";
+			this.LblToplamKategori.Size = new System.Drawing.Size(85, 29);
+			this.LblToplamKategori.TabIndex = 1;
+			this.LblToplamKategori.Text = "label2";
 			// 
 			// panel2
 			// 
@@ -312,6 +312,7 @@
 			this.Name = "Frmistatistik";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "İstatistikler";
+			this.Load += new System.EventHandler(this.Frmistatistik_Load);
 			this.panel1.ResumeLayout(false);
 			this.panel1.PerformLayout();
 			this.panel2.ResumeLayout(false);
@@ -335,7 +336,7 @@
 		#endregion
 
 		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label LblToplamKategori;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Panel panel2;
 		private System.Windows.Forms.Label label3;
--- C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.cs
+++ C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Urun_Takip
 {
 	public partial class Frmistatistik : Form
 	{
+		SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-U5FIOK2\\SQLEXPRESS;Initial Catalog=DbUrun;Integrated Security=True");
+
 		public Frmistatistik()
 		{
 			InitializeComponent();
+		}
+
+		private void Frmistatistik_Load(object sender, EventArgs e)
+		{
+			//Toplam Kategori Sayısı
+			baglanti.Open();
+			SqlCommand komut1 = new SqlCommand("Select COUNT(*) From TBLKATEGORI", baglanti);
+			SqlDataReader dr = komut1.ExecuteReader();
+			while(dr.Read())
+			{
+				LblToplamKategori.Text = dr[0].ToString();
+			}
+			baglanti.Close();
 		}
 	}
 }
