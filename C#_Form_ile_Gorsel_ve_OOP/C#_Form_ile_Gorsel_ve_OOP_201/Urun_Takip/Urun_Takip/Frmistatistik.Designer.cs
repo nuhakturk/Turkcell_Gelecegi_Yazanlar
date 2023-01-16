5ee7bfe1f7493741db7701843386c95aa73170f9
Nuh Akturk <nuhar42@gmail.com>
2023-01-16 23:54:51 +0300

    En Yuksek ve En Dusuk Stoklu Urunler

--- C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.Designer.cs
+++ C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.Designer.cs
@@ -35,16 +35,16 @@
 			this.LblToplamUrun = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.panel3 = new System.Windows.Forms.Panel();
-			this.label5 = new System.Windows.Forms.Label();
+			this.LblBeyazEsya = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
 			this.panel4 = new System.Windows.Forms.Panel();
-			this.label7 = new System.Windows.Forms.Label();
+			this.LblKucukEvAleti = new System.Windows.Forms.Label();
 			this.label8 = new System.Windows.Forms.Label();
 			this.panel5 = new System.Windows.Forms.Panel();
-			this.label9 = new System.Windows.Forms.Label();
+			this.LblEnYuksekStok = new System.Windows.Forms.Label();
 			this.label10 = new System.Windows.Forms.Label();
 			this.panel6 = new System.Windows.Forms.Panel();
-			this.label11 = new System.Windows.Forms.Label();
+			this.LblEnDusukStok = new System.Windows.Forms.Label();
 			this.label12 = new System.Windows.Forms.Label();
 			this.panel7 = new System.Windows.Forms.Panel();
 			this.label13 = new System.Windows.Forms.Label();
@@ -123,80 +123,80 @@
 			// panel3
 			// 
 			this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(89)))), ((int)(((byte)(182)))));
-			this.panel3.Controls.Add(this.label5);
+			this.panel3.Controls.Add(this.LblBeyazEsya);
 			this.panel3.Controls.Add(this.label6);
 			this.panel3.Location = new System.Drawing.Point(554, 12);
 			this.panel3.Name = "panel3";
 			this.panel3.Size = new System.Drawing.Size(265, 177);
 			this.panel3.TabIndex = 3;
 			// 
-			// label5
+			// LblBeyazEsya
 			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-			this.label5.Location = new System.Drawing.Point(100, 108);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(85, 29);
-			this.label5.TabIndex = 1;
-			this.label5.Text = "label5";
+			this.LblBeyazEsya.AutoSize = true;
+			this.LblBeyazEsya.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblBeyazEsya.Location = new System.Drawing.Point(100, 108);
+			this.LblBeyazEsya.Name = "LblBeyazEsya";
+			this.LblBeyazEsya.Size = new System.Drawing.Size(85, 29);
+			this.LblBeyazEsya.TabIndex = 1;
+			this.LblBeyazEsya.Text = "label5";
 			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(45, 44);
+			this.label6.Location = new System.Drawing.Point(64, 44);
 			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(195, 22);
+			this.label6.Size = new System.Drawing.Size(157, 22);
 			this.label6.TabIndex = 0;
-			this.label6.Text = "Toplam Kategori Sayısı";
+			this.label6.Text = "Beyaz Eşya Sayısı";
 			// 
 			// panel4
 			// 
 			this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
-			this.panel4.Controls.Add(this.label7);
+			this.panel4.Controls.Add(this.LblKucukEvAleti);
 			this.panel4.Controls.Add(this.label8);
 			this.panel4.Location = new System.Drawing.Point(826, 12);
 			this.panel4.Name = "panel4";
 			this.panel4.Size = new System.Drawing.Size(265, 177);
 			this.panel4.TabIndex = 4;
 			// 
-			// label7
+			// LblKucukEvAleti
 			// 
-			this.label7.AutoSize = true;
-			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-			this.label7.Location = new System.Drawing.Point(100, 108);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(85, 29);
-			this.label7.TabIndex = 1;
-			this.label7.Text = "label7";
+			this.LblKucukEvAleti.AutoSize = true;
+			this.LblKucukEvAleti.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblKucukEvAleti.Location = new System.Drawing.Point(100, 108);
+			this.LblKucukEvAleti.Name = "LblKucukEvAleti";
+			this.LblKucukEvAleti.Size = new System.Drawing.Size(85, 29);
+			this.LblKucukEvAleti.TabIndex = 1;
+			this.LblKucukEvAleti.Text = "label7";
 			// 
 			// label8
 			// 
 			this.label8.AutoSize = true;
-			this.label8.Location = new System.Drawing.Point(45, 44);
+			this.label8.Location = new System.Drawing.Point(43, 44);
 			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(195, 22);
+			this.label8.Size = new System.Drawing.Size(199, 22);
 			this.label8.TabIndex = 0;
-			this.label8.Text = "Toplam Kategori Sayısı";
+			this.label8.Text = "Küçük Ev Aletleri Sayısı";
 			// 
 			// panel5
 			// 
 			this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(115)))), ((int)(((byte)(158)))));
-			this.panel5.Controls.Add(this.label9);
 			this.panel5.Controls.Add(this.label10);
+			this.panel5.Controls.Add(this.label15);
 			this.panel5.Location = new System.Drawing.Point(826, 195);
 			this.panel5.Name = "panel5";
 			this.panel5.Size = new System.Drawing.Size(265, 177);
 			this.panel5.TabIndex = 8;
 			// 
-			// label9
+			// LblEnYuksekStok
 			// 
-			this.label9.AutoSize = true;
-			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-			this.label9.Location = new System.Drawing.Point(100, 108);
-			this.label9.Name = "label9";
-			this.label9.Size = new System.Drawing.Size(85, 29);
-			this.label9.TabIndex = 1;
-			this.label9.Text = "label9";
+			this.LblEnYuksekStok.AutoSize = true;
+			this.LblEnYuksekStok.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblEnYuksekStok.Location = new System.Drawing.Point(100, 108);
+			this.LblEnYuksekStok.Name = "LblEnYuksekStok";
+			this.LblEnYuksekStok.Size = new System.Drawing.Size(85, 29);
+			this.LblEnYuksekStok.TabIndex = 1;
+			this.LblEnYuksekStok.Text = "label9";
 			// 
 			// label10
 			// 
@@ -210,22 +210,22 @@
 			// panel6
 			// 
 			this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(165)))), ((int)(((byte)(166)))));
-			this.panel6.Controls.Add(this.label11);
 			this.panel6.Controls.Add(this.label12);
+			this.panel6.Controls.Add(this.label13);
 			this.panel6.Location = new System.Drawing.Point(554, 195);
 			this.panel6.Name = "panel6";
 			this.panel6.Size = new System.Drawing.Size(265, 177);
 			this.panel6.TabIndex = 7;
 			// 
-			// label11
+			// LblEnDusukStok
 			// 
-			this.label11.AutoSize = true;
-			this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-			this.label11.Location = new System.Drawing.Point(100, 108);
-			this.label11.Name = "label11";
-			this.label11.Size = new System.Drawing.Size(99, 29);
-			this.label11.TabIndex = 1;
-			this.label11.Text = "label11";
+			this.LblEnDusukStok.AutoSize = true;
+			this.LblEnDusukStok.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.LblEnDusukStok.Location = new System.Drawing.Point(100, 108);
+			this.LblEnDusukStok.Name = "LblEnDusukStok";
+			this.LblEnDusukStok.Size = new System.Drawing.Size(99, 29);
+			this.LblEnDusukStok.TabIndex = 1;
+			this.LblEnDusukStok.Text = "label11";
 			// 
 			// label12
 			// 
@@ -239,7 +239,7 @@
 			// panel7
 			// 
 			this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
-			this.panel7.Controls.Add(this.label13);
+			this.panel7.Controls.Add(this.LblEnDusukStok);
 			this.panel7.Controls.Add(this.label14);
 			this.panel7.Location = new System.Drawing.Point(283, 195);
 			this.panel7.Name = "panel7";
@@ -259,16 +259,16 @@
 			// label14
 			// 
 			this.label14.AutoSize = true;
-			this.label14.Location = new System.Drawing.Point(45, 44);
+			this.label14.Location = new System.Drawing.Point(56, 44);
 			this.label14.Name = "label14";
-			this.label14.Size = new System.Drawing.Size(195, 22);
+			this.label14.Size = new System.Drawing.Size(187, 22);
 			this.label14.TabIndex = 0;
-			this.label14.Text = "Toplam Kategori Sayısı";
+			this.label14.Text = "En Düşük Stoklu Ürün";
 			// 
 			// panel8
 			// 
 			this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
-			this.panel8.Controls.Add(this.label15);
+			this.panel8.Controls.Add(this.LblEnYuksekStok);
 			this.panel8.Controls.Add(this.label16);
 			this.panel8.Location = new System.Drawing.Point(12, 195);
 			this.panel8.Name = "panel8";
@@ -288,11 +288,11 @@
 			// label16
 			// 
 			this.label16.AutoSize = true;
-			this.label16.Location = new System.Drawing.Point(45, 44);
+			this.label16.Location = new System.Drawing.Point(52, 44);
 			this.label16.Name = "label16";
 			this.label16.Size = new System.Drawing.Size(195, 22);
 			this.label16.TabIndex = 0;
-			this.label16.Text = "Toplam Kategori Sayısı";
+			this.label16.Text = "En Yüksek Stoklu Ürün";
 			// 
 			// Frmistatistik
 			// 
@@ -342,16 +342,16 @@
 		private System.Windows.Forms.Label LblToplamUrun;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Panel panel3;
-		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label LblBeyazEsya;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.Panel panel4;
-		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label LblKucukEvAleti;
 		private System.Windows.Forms.Label label8;
 		private System.Windows.Forms.Panel panel5;
-		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label LblEnYuksekStok;
 		private System.Windows.Forms.Label label10;
 		private System.Windows.Forms.Panel panel6;
-		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label LblEnDusukStok;
 		private System.Windows.Forms.Label label12;
 		private System.Windows.Forms.Panel panel7;
 		private System.Windows.Forms.Label label13;
--- C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.cs
+++ C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_201/Urun_Takip/Urun_Takip/Frmistatistik.cs
@@ -33,13 +33,53 @@ namespace Urun_Takip
 			}
 			baglanti.Close();
 
-			//Toplam Kategori Sayısı
+			//Toplam Ürün Sayısı
 			baglanti.Open();
 			SqlCommand komut2 = new SqlCommand("Select COUNT(*) From TBLURUNLER", baglanti);
 			SqlDataReader dr2 = komut2.ExecuteReader();
 			while (dr2.Read())
 			{
 				LblToplamUrun.Text = dr2[0].ToString();
+			}
+			baglanti.Close();
+
+			//Toplam Beyaz Eşya Sayısı
+			baglanti.Open();
+			SqlCommand komut3 = new SqlCommand("Select COUNT(*) From TBLURUNLER where Kategori=(Select ID From TBLKATEGORI where Ad = 'Beyaz Eşya')", baglanti);
+			SqlDataReader dr3 = komut3.ExecuteReader();
+			while (dr3.Read())
+			{
+				LblBeyazEsya.Text = dr3[0].ToString();
+			}
+			baglanti.Close();
+
+			//Toplam Küçük Ev Aleti Sayısı
+			baglanti.Open();
+			SqlCommand komut4 = new SqlCommand("Select COUNT(*) From TBLURUNLER where Kategori=(Select ID From TBLKATEGORI where Ad = 'Küçük Ev Aletleri')", baglanti);
+			SqlDataReader dr4 = komut4.ExecuteReader();
+			while (dr4.Read())
+			{
+				LblKucukEvAleti.Text = dr4[0].ToString();
+			}
+			baglanti.Close();
+
+			//En Yüksek Stoklu Ürün
+			baglanti.Open();
+			SqlCommand komut5 = new SqlCommand("Select * From TBLURUNLER where Stok=(Select Max(Stok) From TBLURUNLER)", baglanti);
+			SqlDataReader dr5 = komut5.ExecuteReader();
+			while (dr5.Read())
+			{
+				LblEnYuksekStok.Text = dr5["UrunAd"].ToString();
+			}
+			baglanti.Close();
+
+			//En Düşük Stoklu Ürün
+			baglanti.Open();
+			SqlCommand komut6 = new SqlCommand("Select * From TBLURUNLER where Stok=(Select Min(Stok) From TBLURUNLER)", baglanti);
+			SqlDataReader dr6 = komut6.ExecuteReader();
+			while (dr6.Read())
+			{
+				LblEnDusukStok.Text = dr6["UrunAd"].ToString();
 			}
 			baglanti.Close();
 		}
