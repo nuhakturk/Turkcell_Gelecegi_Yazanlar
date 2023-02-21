c6effb8b53cfa204cd28ade9a2d6d5c2056a14cc
Nuh Akturk <nuhar42@gmail.com>
2023-02-22 00:31:48 +0300

    Sezar Sifreleme Uygulamasi

---
+++ C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_501/Sezar_Sifreleme/Sezar_Sifreleme/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sezar_Sifreleme
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//merhaba m e r h a b a
+			string veri = "";
+			veri = textBox1.Text;
+			char[] karakterler = veri.ToCharArray();
+
+			foreach (char eleman in karakterler)
+			{
+				textBox2.Text += Convert.ToChar(eleman + 3).ToString();
+			}
+		}
+	}
+}
