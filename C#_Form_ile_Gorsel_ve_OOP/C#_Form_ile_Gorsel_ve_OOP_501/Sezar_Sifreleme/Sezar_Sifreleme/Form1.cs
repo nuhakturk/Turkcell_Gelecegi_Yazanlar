2f3df703627f16c5959490904a2fb7d5bdbe043f
Nuh Akturk <nuhar42@gmail.com>
2023-02-22 00:37:40 +0300

    Sezar Sifre Cozme Uygulamasi

--- C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_501/Sezar_Sifreleme/Sezar_Sifreleme/Form1.cs
+++ C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_501/Sezar_Sifreleme/Sezar_Sifreleme/Form1.cs
@@ -19,7 +19,7 @@ namespace Sezar_Sifreleme
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			//merhaba m e r h a b a
+			//merhaba --> m e r h a b a
 			string veri = "";
 			veri = textBox1.Text;
 			char[] karakterler = veri.ToCharArray();
@@ -27,6 +27,19 @@ namespace Sezar_Sifreleme
 			foreach (char eleman in karakterler)
 			{
 				textBox2.Text += Convert.ToChar(eleman + 3).ToString();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string sifre = "";
+			sifre = textBox2.Text;
+			char[] karakterler= sifre.ToCharArray();
+
+			//def --> d e f
+			foreach (char eleman in karakterler)
+			{
+				textBox1.Text += Convert.ToChar(eleman - 3).ToString();
 			}
 		}
 	}
