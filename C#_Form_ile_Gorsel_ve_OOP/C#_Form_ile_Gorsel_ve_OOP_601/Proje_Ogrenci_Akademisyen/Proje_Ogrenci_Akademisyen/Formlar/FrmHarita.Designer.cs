c544eb25a2c111111c8f3b373e528cbed4332403
Nuh Akturk <nuhar42@gmail.com>
2023-03-03 16:21:31 +0300

    Harita Formu Tasarimi

--- C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_601/Proje_Ogrenci_Akademisyen/Proje_Ogrenci_Akademisyen/Formlar/FrmHarita.Designer.cs
+++ C#_Form_ile_Gorsel_ve_OOP/C#_Form_ile_Gorsel_ve_OOP_601/Proje_Ogrenci_Akademisyen/Proje_Ogrenci_Akademisyen/Formlar/FrmHarita.Designer.cs
@@ -28,21 +28,276 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.panel7 = new System.Windows.Forms.Panel();
+			this.label7 = new System.Windows.Forms.Label();
+			this.panel8 = new System.Windows.Forms.Panel();
+			this.label8 = new System.Windows.Forms.Label();
+			this.panel9 = new System.Windows.Forms.Panel();
+			this.label9 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.panel5.SuspendLayout();
+			this.panel6.SuspendLayout();
+			this.panel7.SuspendLayout();
+			this.panel8.SuspendLayout();
+			this.panel9.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(195)))), ((int)(((byte)(18)))));
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 100);
+			this.panel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(37, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(170, 29);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Bölüm Listesi";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(229)))), ((int)(((byte)(56)))));
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Location = new System.Drawing.Point(218, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(200, 100);
+			this.panel2.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(46, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(147, 29);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Yeni Bölüm";
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(203)))), ((int)(((byte)(196)))));
+			this.panel3.Controls.Add(this.label3);
+			this.panel3.Location = new System.Drawing.Point(424, 12);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(200, 100);
+			this.panel3.TabIndex = 1;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(34, 38);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(166, 29);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Notlar Formu";
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(159)))), ((int)(((byte)(31)))));
+			this.panel4.Controls.Add(this.label4);
+			this.panel4.Location = new System.Drawing.Point(12, 118);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(200, 100);
+			this.panel4.TabIndex = 2;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(30, 38);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(188, 29);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "Öğrenci Formu";
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(203)))), ((int)(((byte)(56)))));
+			this.panel5.Controls.Add(this.label5);
+			this.panel5.Location = new System.Drawing.Point(218, 118);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(200, 100);
+			this.panel5.TabIndex = 3;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(34, 38);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(170, 29);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "Öğrenci Kayıt";
+			// 
+			// panel6
+			// 
+			this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(137)))), ((int)(((byte)(167)))));
+			this.panel6.Controls.Add(this.label6);
+			this.panel6.Location = new System.Drawing.Point(424, 118);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(200, 100);
+			this.panel6.TabIndex = 3;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label6.ForeColor = System.Drawing.Color.White;
+			this.label6.Location = new System.Drawing.Point(38, 38);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(151, 29);
+			this.label6.TabIndex = 0;
+			this.label6.Text = "Ders Listesi";
+			// 
+			// panel7
+			// 
+			this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(90)))), ((int)(((byte)(36)))));
+			this.panel7.Controls.Add(this.label7);
+			this.panel7.Location = new System.Drawing.Point(12, 224);
+			this.panel7.Name = "panel7";
+			this.panel7.Size = new System.Drawing.Size(200, 100);
+			this.panel7.TabIndex = 4;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label7.ForeColor = System.Drawing.Color.White;
+			this.label7.Location = new System.Drawing.Point(46, 38);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(128, 29);
+			this.label7.TabIndex = 0;
+			this.label7.Text = "Yeni Ders";
+			// 
+			// panel8
+			// 
+			this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(148)))), ((int)(((byte)(50)))));
+			this.panel8.Controls.Add(this.label8);
+			this.panel8.Location = new System.Drawing.Point(218, 224);
+			this.panel8.Name = "panel8";
+			this.panel8.Size = new System.Drawing.Size(200, 100);
+			this.panel8.TabIndex = 5;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label8.ForeColor = System.Drawing.Color.White;
+			this.label8.Location = new System.Drawing.Point(57, 38);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(96, 29);
+			this.label8.TabIndex = 0;
+			this.label8.Text = "Yardım";
+			// 
+			// panel9
+			// 
+			this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(82)))), ((int)(((byte)(221)))));
+			this.panel9.Controls.Add(this.label9);
+			this.panel9.Location = new System.Drawing.Point(424, 224);
+			this.panel9.Name = "panel9";
+			this.panel9.Size = new System.Drawing.Size(200, 100);
+			this.panel9.TabIndex = 5;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label9.ForeColor = System.Drawing.Color.White;
+			this.label9.Location = new System.Drawing.Point(49, 38);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(124, 29);
+			this.label9.TabIndex = 0;
+			this.label9.Text = "Çıkış Yap";
 			// 
 			// FrmHarita
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(551, 322);
+			this.ClientSize = new System.Drawing.Size(636, 334);
+			this.Controls.Add(this.panel9);
+			this.Controls.Add(this.panel8);
+			this.Controls.Add(this.panel7);
+			this.Controls.Add(this.panel6);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "FrmHarita";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "FrmHarita";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
+			this.panel5.ResumeLayout(false);
+			this.panel5.PerformLayout();
+			this.panel6.ResumeLayout(false);
+			this.panel6.PerformLayout();
+			this.panel7.ResumeLayout(false);
+			this.panel7.PerformLayout();
+			this.panel8.ResumeLayout(false);
+			this.panel8.PerformLayout();
+			this.panel9.ResumeLayout(false);
+			this.panel9.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Panel panel7;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Panel panel8;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Panel panel9;
+		private System.Windows.Forms.Label label9;
 	}
 }
